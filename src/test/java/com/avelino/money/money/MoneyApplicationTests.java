d9a3ddd9beec57c2c6cdc3992b184a30bb901791
Carlos Avelino <carlos.avelino2.0@gmail.com>
2020-04-02 20:18:10 -0300

    Project created

---
+++ src/test/java/com/avelino/money/money/MoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avelino.money.money;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
